45bbe499700d7e9b3d2d96d194648ac245469043
KmolYuan <pyslvs@gmail.com>
2019-12-02 10:58:44 +0800

    Update HW5 code.

--- homework5-matlab/main.m
+++ homework5-matlab/main.m
@@ -3,15 +3,15 @@ clc; clear;
 
 %% Optimization
 obj = @(x) x(1) + x(2);
-op = optimoptions('fmincon', 'Algorithm', 'sqp');
-[x, fval, flag, out] = fmincon(obj, [0, 0], [], [], [], [], [], [], @fosm, op);
+% op = optimoptions('fmincon', 'Algorithm', 'sqp');
+% [x, fval, flag, out] = fmincon(obj, [0, 0], [], [], [], [], [], [], @fosm, op);
+[x, fval, flag, out] = ga(obj, 2, [], [], [], [], [], [], @fosm);
 
 %% Result
 if flag == 1
     fprintf("algorithm: %s\n", out.algorithm);
     fprintf("(iter: %d, step: %i)\n", out.iterations, out.stepsize);
     fprintf("f(%.10f, %.10f) = %.10f\n", x, fval);
-    fprintf("min weight: %.10f kg\n", fval * rho);
 else
     fprintf("Error: %d\n", flag);
 end
