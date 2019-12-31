0a6dac955cc9f5c29b0cc7a5b39d84fa17e962b8
KmolYuan <pyslvs@gmail.com>
2019-12-31 23:05:05 +0800

    Add report markdown.

--- compile.sh
+++ compile.sh
@@ -3,11 +3,16 @@
 # sh compile.sh FILE_NUMBER
 
 BASEDIR=$(readlink -f "$(dirname "$(readlink -f "${0}")")")
-if [ "${1}" == "all" ]; then
-  FILE=($(ls *.md))
+if [ "${1}" == "all" ]
+then
+  FILE=($(ls *.md) "${BASEDIR}/group.md")
+elif [ "${1}" == "g" ]
+then
+  FILE=("${BASEDIR}/group.md")
 else
   FILE=("${BASEDIR}/homework${1}.md")
 fi
-for f in "${FILE[@]}"; do
+for f in "${FILE[@]}"
+do
   pandoc ${f} -o $(basename -- "${f%.*}").pdf -N -M link-citations=true --bibliography=refer.bib --csl=ieee.csl
 done
---
+++ group.md
@@ -0,0 +1,11 @@
+\section*{\center Final Report}
+
+Class: ME7129 Optimization in Engineering, National Taiwan University.
+
+Student: Yuan Chang
+
+Due date: 2020-01-07
+
+This PDF is generated from Markdown[@group-md], scripting in Matlab[@group-ml].
+
+# Bibliography
